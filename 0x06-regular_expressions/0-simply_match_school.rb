1503d73d683214c2d8032cef5b8bdf5d9f0379ea
Bakry200 <bakryalfadul@gmail.com>
2024-05-07 13:06:55 -0700

    hj

---
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -0,0 +1,15 @@
+#!/usr/bin/env ruby
+def regex_match(argument)
+  regex = /School/
+  if argument.match?(regex)
+    puts "Match found!"
+  else
+    puts "No match found."
+  end
+end
+Check if an argument is provided
+if ARGV.empty?
+  puts "Please provide an argument."
+else
+  regex_match(ARGV[0])
+end
---
+++ 0x06-regular_expressions/7-OMG_WHY_ARE_YOU_SHOUTING.rb
@@ -0,0 +1,9 @@
+#!/bin/bash
+# Example usage of regex to match capital letters
+
+text="Hello World"
+if [[ $text =~ [[:upper:]] ]]; then
+        echo "The text contains capital letters."
+else
+  echo "The text does not contain capital letters."
+  fi
