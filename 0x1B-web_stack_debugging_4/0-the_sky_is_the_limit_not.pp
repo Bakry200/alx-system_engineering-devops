baea7c1c1901abe54143b3cf283e61f607a92d5b
Bakry200 <bakryalfadul@gmail.com>
2024-08-22 22:54:21 +0000

    jk

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -3,3 +3,4 @@ exec { 'fix--for-nginx':
   command => "bash -c \"sed -iE 's/^ULIMIT=.*/ULIMIT=\\\"-n 8192\\\"/' \
 /etc/default/nginx; service nginx restart\"",
   path    => '/usr/bin:/usr/sbin:/bin'
+}
